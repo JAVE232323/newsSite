1e9fc4aebfbad74a95dee9622000b0f5e538e3ca
JAVE232323 <a.tyulmenev@yandex.ru>
2024-07-28 17:35:25 +0300

    preliminary working model

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const router = require('./routes/index')
+const cors = require('cors')
 
-const PORT = process.env.PORT || 5432
+const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('api', router)
+
+
 
 const start = async () => {
     try{
         await sequelize.authenticate
         await sequelize.sync
         app.listen(PORT, () => 
-            console.log("Server started on port 5432")
+            console.log('Server started on port ${PORT}')
         )
     }
     catch(e){
